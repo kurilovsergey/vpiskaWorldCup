c48b21d291e09a1eb63d8f2fac88deeec300d233
Kurilov Sergey <47578235+kurilovsergey@users.noreply.github.com>
2026-07-05 19:16:10 +0300

    Update matches.js

--- matches.js
+++ matches.js
@@ -1,5 +1,7 @@
+
 const MATCHES = [
   { num: 77, teams: "France vs Sweden", date: "30 JUNE 26", time: "17:00", stadium: "New York/New Jersey Stadium", entrance: "A/B" },
   { num: 80, teams: "England vs DR Congo", date: "01 JULY 26", time: "12:00", stadium: "Atlanta Stadium", entrance: "A/B/C" },
   { num: 86, teams: "Argentina vs Cape Verde", date: "03 JULY 26", time: "18:00", stadium: "Miami Stadium", entrance: "A/B" },
+  { num: 91, teams: "Brazil vs Norway", date: "05 JULY 26", time: "16:00", stadium: "New York/New Jersey Stadium", entrance: "1/2" },
 ];
